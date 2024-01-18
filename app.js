4000d89ca0b705987ee08ef2fc9836ad0d7e8925
sutiwatNG <sutiwatngampiam@gmail.com>
2024-01-18 21:13:58 +0700

    javascriptb basic

---
+++ app.js
@@ -0,0 +1,22 @@
+ //สร้างตัวแปร
+let name = "golf"//กำหนดค่าตัวแปร
+
+//เช็ค debug ค่าต่าง ๆ แต่จะไม่แสดงผลในหน้าเว็บ
+console.log("aaaa")
+
+//แสดงข้อความ ตัวเลข ตัวแปร หรือแท็ก HTML ในหน้าเว็บ
+document.write(name) 
+
+//แจ้งเตือนผู้ใช้
+alert("ระวังนะ")
+
+//คัวแปร array
+let age = Array(15,17,16,13,12)
+
+document.write("<p>",age,"</p>")
+document.write("<p>",age[2],"</p>")
+console.log(age)
+//--------
+
+
+
